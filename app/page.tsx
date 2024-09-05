d6cef0bf7bba47b2e1db2d8c620981491c32dd5e
twu148 <twu148@ucr.edu>
2024-09-05 14:06:43 -0700

    changing spacing

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,16 @@ export default function Page() {
         tingtingtingtingting
       </h1>
       <p className="mb-4">
-        {`Hi there, I'm Ting!
-        I'm an undergraduate Computer Science student at UC Riverside with a passion for creating.
-        With hands-on experience in web and gave development, I enjoy creating user-friendly and visually appealing experiences. I’ve also had the opportunity to lead and collaborate on various projects, which has strengthened my problem-solving skills and attention to detail. 
-        Outside of programming, I have a deep appreciation for art, design, and music, which often influences my approach to creating digital experiences. I'm always eager to learn, adapt, and take on new challenges in the tech world.`}
+        {`Hi there, I'm Ting!\n
+        I'm an undergraduate Computer Science student at UC Riverside 
+        with a passion for creating.\n
+        With hands-on experience in web and gave development, 
+        I enjoy creating user-friendly and visually appealing experiences. 
+        I’ve also had the opportunity to lead and collaborate on various projects, 
+        which has strengthened my problem-solving skills and attention to detail.\n 
+        Outside of programming, I have a deep appreciation for art, design, 
+        and music, which often influences my approach to creating digital experiences. 
+        I'm always eager to learn, adapt, and take on new challenges in the tech world.`}
       </p>
       <div className="my-8">
         <BlogPosts />
